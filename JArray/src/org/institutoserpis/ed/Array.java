169cb929a3a5dd6e01ac5a8855c637e14b78dd6a
DanielRomero <dam.dromero@institutoserpis.org>
2015-10-26 12:12:42 +0100

    array

---
+++ JArray/src/org/institutoserpis/ed/Array.java
@@ -0,0 +1,52 @@
+package org.institutoserpis.ed;
+
+public class Array {
+
+	public static void main(String[] args) {
+
+		int[] v = new int[]{5, 9, 7, 12, 21};
+		
+		for (int index=0; index < v.length; index++)
+			System.out.println("elemento con index=" + index + " vale " + v[index]);
+		
+		System.out.println("suma del array=" + suma(v));
+		System.out.println("suma de otro array=" + suma(new int[]{14, 24}));
+		System.out.println("suma array vacio=" + suma(new int[]{}));
+		
+		System.out.println("el valor menor en v es: " + menor(v));
+		System.out.println("el índice de 12 en v es: " + indexOf(v, 12));
+		
+	}
+	
+	public static int suma(int[] v) {
+		// número de elementos del vector: v.length
+		int suma = 0;
+		
+//		for (int index=0; index < v.length; index++)
+//			suma = suma + v[index];
+		
+		for (int item : v)
+			suma = suma + item;
+		
+		return suma;
+	}
+	
+	public static int menor(int[] v) {
+		int menor = v[0];
+		for (int index = 1; index < v.length; index++)
+			if (v[index] < menor)
+				menor = v[index];
+		return menor;
+	}
+	
+	public static int indexOf(int[] v, int item) {
+		int index=0;
+		while (index < v.length && v[index]!=item)
+			index++;
+		if (index == v.length)
+			return -1;
+		return index;
+	}
+	
+
+}
---
+++ JArray/src/org/institutoserpis/ed/ArrayTest.java
@@ -0,0 +1,23 @@
+package org.institutoserpis.ed;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArrayTest {
+
+	@Test
+	public void menor() {
+		assertEquals(5, Array.menor(new int[]{5,9,6,14}));
+		assertEquals(3, Array.menor(new int[]{5,9,3,14}));
+		assertEquals(7, Array.menor(new int[]{9,8,12,7}));
+	}
+
+	@Test
+	public void indexOf() {
+		assertEquals(0, Array.indexOf(new int[]{7,21,9,13}, 7));
+		assertEquals(1, Array.indexOf(new int[]{7,21,9,13}, 21));
+		assertEquals(3, Array.indexOf(new int[]{7,21,9,13}, 13));
+	}
+
+}
