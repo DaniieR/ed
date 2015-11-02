2b50b277468d23a2071fdb8151c5fcf19b58a901
DanielRomero <dam.dromero@institutoserpis.org>
2015-11-02 12:12:39 +0100

    array

--- JArray/src/org/institutoserpis/ed/Array.java
+++ JArray/src/org/institutoserpis/ed/Array.java
@@ -53,16 +53,31 @@ public class Array {
 		return -1;
 	}
 	public static void sort(int[] v){
+//		for (int stepIndex = 0; stepIndex < v.length -1; stepIndex++){
+//			int minIndex = stepIndex ;
+//			for (int index = stepIndex + 1; index < v.length; index++){
+//				if (v[index] < v[minIndex])
+//					minIndex = index;
+//			int aux = v[stepIndex];
+//			v[stepIndex] = v[minIndex];
+//			v[minIndex] = aux;
+//			}
+//		}
 		for (int stepIndex = 0; stepIndex < v.length -1; stepIndex++){
-			int minIndex = stepIndex ;
-			for (int index = stepIndex + 1; index < v.length; index++){
-				if (v[index] < v[minIndex])
-					minIndex = index;
-			int aux = v[stepIndex];
-			v[stepIndex] = v[minIndex];
-			v[minIndex] = aux;
+			int minIndex = minIndex (v, stepIndex) ;
+			swap(v, stepIndex, minIndex);
 			}
 		}
+		private static int minIndex(int[] v, int initialIndex){
+			int minIndex = initialIndex ;
+			for (int index = initialIndex + 1; index < v.length; index++)
+				if (v[index] < v[minIndex])
+					minIndex = index;
+			return minIndex;
+		}
+		private static void swap(int[] v, int oneIndex, int otherIndex){
+			int aux = v[oneIndex];
+			v[oneIndex] = v[otherIndex];
+			v[otherIndex] = aux;
+		}
 	}
-
-}
