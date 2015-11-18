eb196b5158c52db6bfe17b45ef3f81958cb25267
DaniieR <dam.dromero@institutoserpis.org>
2015-11-18 09:52:25 +0100

    Monodevelop

---
+++ PBingo/PBingo/MainWindow.cs
@@ -0,0 +1,18 @@
+using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{	
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		Button button = new Button ();
+		Button.label = 
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
