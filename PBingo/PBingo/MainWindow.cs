def1c74ba8ae9570f46849b29c2f768379511c0b
DaniieR <dam.dromero@institutoserpis.org>
2015-12-02 08:44:12 +0100

    Monodevelop

--- PBingo/PBingo/MainWindow.cs
+++ PBingo/PBingo/MainWindow.cs
@@ -13,14 +13,17 @@ public partial class MainWindow: Gtk.Window
 		random = new Random ();
 		Table table = new Table (9 , 10, true);
 		List<Button> buttons = new List<Button> ();
+		List<int> numeros = new List<int> ();
 		for (uint index = 0; index < 90; index++){
 			Button button = new Button ();
-			button.Label = (index + 1).ToString();
+			int numero = (int)index + 1;
+			button.Label = numero.ToString();
 			button.Visible = true;
 			uint row= index / 10;
 			uint column=index % 10;
 			table.Attach (button, column, column + 1 ,row ,row + 1);
 			buttons.Add (button);
+			numeros.Add (numero);
 		}
 		//		//Opción 2
 		//		for (uint row = 0; row < 9; row++)
@@ -34,10 +37,15 @@ public partial class MainWindow: Gtk.Window
 		table.Visible = true;
 		vbox1.Add (table);
 		buttonnumero.Clicked += delegate {
-			int indexAleatorio = random.Next (90);
-			Button button = buttons [indexAleatorio];
+			int indexAleatorio = random.Next (numeros.Count);
+			int numero = numeros [indexAleatorio];
+			labelnumero.Text=numero.ToString();
+			numeros.RemoveAt(indexAleatorio);
+			indexAleatorio++;
+			Button button = buttons [numero-1];
 			button.ModifyBg (StateType.Normal, GREEN_COLOR);
 			Process.Start ("espeak", "-v es " + (indexAleatorio+1));
+			buttonnumero.Sensitive = buttons.Count>0;
 		};
 	}
 
