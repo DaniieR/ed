d51b7e6fca9bf55843cdd3c29adea0d30a687d84
DaniieR <dam.dromero@institutoserpis.org>
2016-03-02 09:50:40 +0100

    SQL

--- GMySql/src/main/java/org/institutoserpis/ed/PruebaCategoria.java
+++ GMySql/src/main/java/org/institutoserpis/ed/PruebaCategoria.java
@@ -1,10 +1,21 @@
 package org.institutoserpis.ed;
-
+import java.sql.*;
 public class PruebaCategoria {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws SQLException {
+		System.out.println("GMySQL Inicio");
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dbprueba","root","sistemas");
+		
+		//Todo lo que quieras hacer en la base de datos...
+		//Leer los registrios de la tabla categoria
+		Statement selectStatement = connection.createStatement();
+		ResultSet resultSet = selectStatement.executeQuery("select * from categoria");
+		while (resultSet.next()){
+			System.out.printf("id=%s nombre=%s \n",resultSet.getObject("id"),resultSet.getObject("nombre"));
+		}
+		selectStatement.close();
+		connection.close();
+		System.out.println("FIN");
 	}
 
 }
